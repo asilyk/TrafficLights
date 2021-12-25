b100d86768134d7ee347dfe84105745ca10ccfc6
Alexander Ilyk <asi112@icloud.com>
2021-12-25 08:03:52 +0300

    Setup button "Start"

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
     }
 
 
