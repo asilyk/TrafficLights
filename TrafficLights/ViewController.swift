214a4f2e8d0098f8aded9457dea736dab920edb6
Alexander Ilyk <asi112@icloud.com>
2021-12-25 16:16:32 +0300

    Setup main storyboard and button logic. Lights are circle only on iPhone 11 screen - need to be fixed

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,14 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: - IB Outlets
+    @IBOutlet private var startButton: UIButton!
 
-    @IBOutlet var startButton: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        startButton.layer.cornerRadius = 10
+    @IBOutlet private var lightsStackView: UIStackView!
+
+    @IBOutlet private var yellowLightView: UIView!
+    @IBOutlet private var redLightView: UIView!
+    @IBOutlet private var greenLightView: UIView!
+
+    //MARK: - Enumerations
+    private enum WhatLightIsOn {
+        case red
+        case yellow
+        case green
+        case none
     }
 
+    //MARK: - Private Properties
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
 
+    private var whatLightIsOn = WhatLightIsOn.none
+
+    //MARK: - Life Cycles Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startButton.layer.cornerRadius = 10
+
+        for lightView in lightsStackView.arrangedSubviews {
+            lightView.layer.cornerRadius = lightView.frame.size.width / 2
+            lightView.alpha = lightIsOff
+        }
+    }
+
+    //MARK: - IB Actions
+    @IBAction private func startButtonPressed() {
+        switch whatLightIsOn {
+        case .none:
+            startButton.setTitle("Next", for: .normal)
+            whatLightIsOn = .red
+            redLightView.alpha = lightIsOn
+
+        case .red:
+            whatLightIsOn = .yellow
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+
+        case .yellow:
+            whatLightIsOn = .green
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+
+        case .green:
+            whatLightIsOn = .red
+            greenLightView.alpha = lightIsOff
+            redLightView.alpha = lightIsOn
+        }
+    }
 }
-
